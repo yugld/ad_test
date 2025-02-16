485846cbd762ddb6cd208cc15cf074999e298a92
yugld <yulliagladysheva@gmail.com>
2025-02-16 12:26:04 +0300

    add 404Page

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -1,10 +1,19 @@
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+
 export const NotFound = () => {
   return (
-    <div className="">
+    <Container
+      maxWidth="lg"
+      component="main"
+      sx={{ display: 'flex', flexDirection: 'column', my: 16, gap: 4 }}
+    >
       <div className="">
-        <h1>Кажется, вы потерялись...</h1>
-        Вернуться на главную страницу
+        <h1>Такой страницы не существует...</h1>
+        <Button href="/" variant="contained">
+          Вернуться на главную
+        </Button>
       </div>
-    </div>
+    </Container>
   );
 };
